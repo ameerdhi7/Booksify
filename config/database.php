5f05a67c3460b2c947820764ce9f523cfa876348
Ameer Dhiaa <dhiaaback@gmail.com>
2020-09-28 12:55:27 +0300

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$DATABASE_URL=parse_url('postgres://djqxotlpidoqio:e92843a009fc809595212b6ba8a860fc31a595478353684da5485b3319c61a4d@ec2-34-234-185-150.compute-1.amazonaws.com:5432/dbodqug30');
 return [
 
     /*
@@ -58,16 +58,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
